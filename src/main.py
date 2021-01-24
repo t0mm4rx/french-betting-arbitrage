a58264eab02b42a54f15e9dc45a34cca6dcb553f
Tom <tom.marx@hotmail.fr>
2021-01-25 00:58:04 +0100

    Better logging

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import arb
 import sys
 import log
 import config
+import traceback
 
 # print(ps3838.get_games({'competition': 'nba', 'sport': 'basketball'}))
 # exit(0)
@@ -21,32 +22,32 @@ for competition in config.competitions:
 		bookmakers['winamax'] = winamax.get_games(competition)
 		log.log("winamax: " + str(bookmakers['winamax']))
 	except:
-		log.log("Cannot crawl winamax: " + sys.exc_info()[0])
+		log.log("Cannot crawl winamax: " + traceback.format_exc())
 	try:
 		bookmakers['pmu'] = pmu.get_games(competition)
 		log.log("pmu: " + str(bookmakers['pmu']))
 	except:
-		log.log("Cannot crawl pmu: " + sys.exc_info()[0])
+		log.log("Cannot crawl pmu: " + traceback.format_exc())
 	try:
 		bookmakers['betclic'] = betclic.get_games(competition)
 		log.log("betclic: " + str(bookmakers['betclic']))
 	except:
-		log.log("Cannot crawl betclic: " + sys.exc_info()[0])
+		log.log("Cannot crawl betclic: " + traceback.format_exc())
 	try:
 		bookmakers['zebet'] = zebet.get_games(competition)
 		log.log("zebet: " + str(bookmakers['zebet']))
 	except:
-		log.log("Cannot crawl zebet: " + sys.exc_info()[0])
+		log.log("Cannot crawl zebet: " + traceback.format_exc())
 	try:
 		bookmakers['netbet'] = netbet.get_games(competition)
 		log.log("netbet: " + str(bookmakers['netbet']))
 	except:
-		log.log("Cannot crawl netbet: " + sys.exc_info()[0])
+		log.log("Cannot crawl netbet: " + traceback.format_exc())
 	try:
 		bookmakers['ps3838'] = ps3838.get_games(competition)
 		log.log("ps3838: " + str(bookmakers['ps3838']))
 	except:
-		log.log("Cannot crawl ps3838: " + sys.exc_info()[0])
+		log.log("Cannot crawl ps3838: " + traceback.format_exc())
 	log.log("-- Competition: {} --".format(competition))
 	for game in bookmakers['winamax']:
 		games = {}
@@ -56,7 +57,7 @@ for competition in config.competitions:
 				if (g):
 					games[bookmaker] = g
 			except:
-				log.log("Error while retrieving games: {}".format(sys.exc_info()[0]))
+				log.log("Error while retrieving games: {}".format(traceback.format_exc()))
 		if (competition["sport"] == "football"):
 			arb.arb_football(games)
 		if (competition["sport"] == "basketball"):
