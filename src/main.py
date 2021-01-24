a270af99bee432e977587b019cdbf6f2feffd7dc
Tom <tom.marx@hotmail.fr>
2021-01-25 00:39:01 +0100

    Separate try/except for each bookmaker and better logs

--- src/main.py
+++ src/main.py
@@ -16,18 +16,37 @@ log.init()
 progress = 0
 for competition in config.competitions:
 	progress += 1
-	# try:
-	bookmakers = {
-		'winamax': winamax.get_games(competition),
-		'pmu': pmu.get_games(competition),
-		'betlic': betclic.get_games(competition),
-		'zebet': zebet.get_games(competition),
-		'netbet': netbet.get_games(competition),
-		'ps3838': ps3838.get_games(competition)
-	}
-	# except:
-	# 	log.log("Error while fetching games: {}".format(sys.exc_info()[0]))
-	# 	continue
+	bookmakers = {}
+	try:
+		bookmakers['winamax'] = winamax.get_games(competition)
+		log.log("winamax: " + str(bookmakers['winamax']))
+	except:
+		log.log("Cannot crawl winamax: " + sys.exc_info()[0])
+	try:
+		bookmakers['pmu'] = pmu.get_games(competition)
+		log.log("pmu: " + str(bookmakers['pmu']))
+	except:
+		log.log("Cannot crawl pmu: " + sys.exc_info()[0])
+	try:
+		bookmakers['betclic'] = betclic.get_games(competition)
+		log.log("betclic: " + str(bookmakers['betclic']))
+	except:
+		log.log("Cannot crawl betclic: " + sys.exc_info()[0])
+	try:
+		bookmakers['zebet'] = zebet.get_games(competition)
+		log.log("zebet: " + str(bookmakers['zebet']))
+	except:
+		log.log("Cannot crawl zebet: " + sys.exc_info()[0])
+	try:
+		bookmakers['netbet'] = netbet.get_games(competition)
+		log.log("netbet: " + str(bookmakers['netbet']))
+	except:
+		log.log("Cannot crawl netbet: " + sys.exc_info()[0])
+	try:
+		bookmakers['ps3838'] = ps3838.get_games(competition)
+		log.log("ps3838: " + str(bookmakers['ps3838']))
+	except:
+		log.log("Cannot crawl ps3838: " + sys.exc_info()[0])
 	log.log("-- Competition: {} --".format(competition))
 	for game in bookmakers['winamax']:
 		games = {}
